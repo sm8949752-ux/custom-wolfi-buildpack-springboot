6fab0b54f0871ebfbb1020feb08592de23727e52
taha2samy <samyt8435@gmail.com>
2026-03-16 14:19:54 +0200

    feat: Add Spring Boot 3.4.3 application with Java 25 on a custom Wolfi buildpack stack.

---
+++ 3-sample-app/src/main/java/com/taha/wolfidemo/DemoApplication.java
@@ -0,0 +1,19 @@
+package com.taha.wolfidemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DemoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+    @GetMapping("/")
+    public String hello() {
+        return "Success! Spring Boot 3.4 on Wolfi Distroless with Java 25. \n";
+    }
+}
